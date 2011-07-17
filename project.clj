bad20715d4b923521e45edaa9f23487c2dd273e3
Sebastián Bernardo Galkin <sgalkin@grantaire.com.ar>
2011-07-17 18:24:01 -0300

    release 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject test-this "0.1.0-SNAPSHOT"
+(defproject test-this "0.1.0"
   :description "Powerful clojure test runner"
   :dependencies [[org.clojure/clojure "1.3.0-beta1"]
                  [org.clojure/tools.namespace "0.1.1"]
